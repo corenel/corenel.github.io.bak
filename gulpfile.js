f35022e791159dd78883daefc82933db0147b96c
corenel <xxdsox@gmail.com>
2016-09-11 12:17:20 +0800

    Modify gulp script.

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,13 @@ var gulp = require('gulp');
 var cleanCSS = require('gulp-clean-css');
 var uglify = require('gulp-uglify');
 var htmlmin = require('gulp-htmlmin');
-var htmlclean = require('gulp-htmlclean');
+// var htmlclean = require('gulp-htmlclean');
 var imagemin = require('gulp-imagemin');
 
 gulp.task('minify-css', function() {
     return gulp.src(["public/**/*.css","!public/**/*.min.css"])
         .pipe(cleanCSS({compatibility: 'ie8'}))
-        .pipe(gulp.dest('./public'));
+        .pipe(gulp.dest('public'));
 });
 
 gulp.task('minify-images', function() {
@@ -19,29 +19,24 @@ gulp.task('minify-images', function() {
             interlaced: true,
             multipass: true
         }))
-        .pipe(gulp.dest('./public/'));
+        .pipe(gulp.dest('public'));
 });
 
 gulp.task('minify-html', function() {
   return gulp.src("public/**/*.html")
-    .pipe(htmlclean())
-    .pipe(htmlmin({
-         removeComments: true,
-         minifyJS: true,
-         minifyCSS: true,
-         minifyURLs: true,
-    }))
-    .pipe(gulp.dest('./public'))
+    // .pipe(htmlclean())
+    .pipe(htmlmin({collapseWhitespace: true}))
+    .pipe(gulp.dest('public'))
 });
 
 gulp.task('minify-js', function() {
     return gulp.src(["public/**/*.js","!public/**/*.min.js"])
         .pipe(uglify())
-        .pipe(gulp.dest('./public'));
+        .pipe(gulp.dest('public'));
 });
 
 gulp.task('default', [
     'minify-html','minify-css','minify-js'
 ],function(){
-    console.log("gulp task ok!");
+    console.log("gulp task done!");
 });
